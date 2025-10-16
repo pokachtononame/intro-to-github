6b1fa72b1d5591a3d2dfcd333e9c7cdac4e11948
pokachtononame <q880534@gmail.com>
2025-10-17 01:41:04 +0300

    Implemented complex numbers operation

--- intro-to-github/main.cpp
+++ intro-to-github/main.cpp
@@ -18,18 +18,40 @@ struct Matrix2i
     Matrix2i operator * (const Matrix2i& op)
     {
         return Matrix2i{ 
-        a1 * op.a1 + b1 * op.a2,
-        a1 * op.b1 + b1 * op.b2,
-        a2 * op.a1 + b2 * op.a2,
-        a2 * op.b1 + b2 * op.b2 
+            a1 * op.a1 + b1 * op.a2,
+            a1 * op.b1 + b1 * op.b2,
+            a2 * op.a1 + b2 * op.a2,
+            a2 * op.b1 + b2 * op.b2 
         };
     }
 
     Matrix2i operator + (const Matrix2i& op)
     {
         return Matrix2i{
-        a1 + op.a1, b1 + op.b1,
-        a2 + op.a2, b2 + op.b2
+            a1 + op.a1, b1 + op.b1,
+            a2 + op.a2, b2 + op.b2
+        };
+    }
+};
+
+struct Complexi
+{
+    int r;
+    int i;
+    void input()
+    {
+        std::cin >> r; std::cout << "i * "; std::cin >> i;
+    }
+    void output()
+    {
+        const char* sign = (i < 0) ? "-" : "+";
+        std::cout << r << " " << *sign << " " << abs(i) << "i\n";
+    }
+    Complexi operator * (const Complexi& op)
+    {
+        return Complexi{
+            r * op.r - i * op.i,
+            r * op.i + i * op.r
         };
     }
 };
@@ -37,46 +59,77 @@ struct Matrix2i
 
 int main()
 {
-    unsigned int action;
-    std::cout << "Select action: \n(1) Get product of integers\n(2) Get sum of integers\n(3) Get product of matrices\n(4) Get sum of matrices\n\n";
-    std::cin >> action;
-    int a, b;
-    Matrix2i mat1;
-    Matrix2i mat2;
-    Matrix2i res;
-    switch (action)
-    {
-    case 1:
-        std::cout << "\n";
-        std::cin >> a;
-        std::cin >> b;
-        std::cout << "\n(1): " << a * b << "\n\n";
-        break;
-    case 2:
-        std::cout << "\n";
-        std::cin >> a;
-        std::cin >> b;
-        std::cout << "\n(2): " << a + b << "\n";
-        break;
-    case 3:
-        std::cout << "\n";
-        mat1.input();
-        std::cout << std::endl;
-        mat2.input();
-        res = mat1 * mat2;
-        std::cout << "(3:)\n";
-        res.output();
-        std::cout << "\n";
-        break;
-    case 4:
-        std::cout << "\n";
-        mat1.input();
-        std::cout << std::endl;
-        mat2.input();
-        res = mat1 + mat2;
-        std::cout << "(4:)\n";
-        res.output();
-        std::cout << "\n";
-        break;
+    while (1) {
+        system("cls");
+        unsigned int action;
+        std::cout <<
+            "Select action: \n"
+            "(1) Get product of integers\n"
+            "(2) Get sum of integers\n"
+            "(3) Get product of matrices\n"
+            "(4) Get sum of matrices\n"
+            "(5) Get product of two complex numbers\n"
+            "(6) Exit\n\n";
+        std::cin >> action;
+        int a, b;
+        Matrix2i mat1;
+        Matrix2i mat2;
+        Matrix2i res;
+        Complexi cint1;
+        Complexi cint2;
+        Complexi complex_res;
+        switch (action)
+        {
+        case 1:
+            std::cout << "\n";
+            std::cin >> a;
+            std::cin >> b;
+            std::cout << "\n(1): " << a * b << "\n\n";
+            break;
+        case 2:
+            std::cout << "\n";
+            std::cin >> a;
+            std::cin >> b;
+            std::cout << "\n(2): " << a + b << "\n";
+            break;
+        case 3:
+            std::cout << "\n";
+            mat1.input();
+            std::cout << std::endl;
+            mat2.input();
+            res = mat1 * mat2;
+            std::cout << "(3:)\n";
+            res.output();
+            std::cout << "\n";
+            break;
+        case 4:
+            std::cout << "\n";
+            mat1.input();
+            std::cout << std::endl;
+            mat2.input();
+            res = mat1 + mat2;
+            std::cout << "(4:)\n";
+            res.output();
+            std::cout << "\n";
+            break;
+        case 5:
+            std::cout << "\n";
+            cint1.input();
+            std::cout << "\n";
+            cint2.input();
+            std::cout << "\n";
+            complex_res = cint1 * cint2;
+            std::cout << "(5:)\n";
+            complex_res.output();
+            std::cout << "\n";
+            break;
+        case 6:
+            return 0;
+            break;
+        default:
+            std::cout << "Error: Invalid input\n";
+            break;
+        }
+        system("pause");
     }
 }
